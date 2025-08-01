f3ae141a899d4f118ec138eae2d6ea96565aca78
DevLia13 <giulia.oliveira@aluno.ifsp.edu.br>
2025-08-01 17:58:10 -0300

    Add files via upload

---
+++ aula6/script.js
@@ -0,0 +1,36 @@
+//comentario de 1 linha
+
+/*Comentário de bloco
+
+testet
+teste
+
+*/
+
+//estrutura de dados
+
+//lista = array/vetores
+
+var numero = 10 // NÃO UTILIZAR vazamento de dados para fora do escopo
+
+let numero = 10 //vai ser alterado usa let
+
+const pi = 3.14159 //NÃO vai ser alterado
+
+let idade = 16
+
+let peso = 56.7
+
+let x = true
+
+let texto = 'isso é um texto'
+
+console.log(pi, idade, peso, x, texto)
+
+//tipos de dados
+
+//real(float) 1.0
+//inteiro(int) 12
+//booleano(boolean) t/f 0-1 - condições
+//string(frases) abacaxi
+//char(caracter) a
